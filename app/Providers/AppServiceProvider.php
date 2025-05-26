e653758a87f5b3fc610262cd7b73665ff93450b8
Davi Herdiansah <daviherdiansahf@gmail.com>
2025-05-26 18:07:48 +0700

    Initial commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Http\Middleware\RoleMiddleware;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->app['router']->aliasMiddleware('role', RoleMiddleware::class);
+        if (env('APP_ENV') !== 'production') {
+        URL::forceScheme('https');
+    }
     }
 }
